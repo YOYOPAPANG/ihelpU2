0d99d7dc33c8ba484b5cc2908f25272892133992
Thanawat <Thanawaat.yoyopapang@mail.kmutt.ac.th>
2021-05-23 23:49:10 +0700

    safsafasf

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -28,8 +28,64 @@ class _RegisterScreenState extends State<RegisterScreen> {
               ),
             );
           }
-          if(snapshot.connectionState == ConnectionState.done){
-
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("สร้างบัญชีผู้ใช้ระบบ"),
+              ),
+              body: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Form(
+                    key: formKey,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Email", style: TextStyle(fontSize: 20)),
+                          TextFormField(
+                            validator: MultiValidator([
+                              RequiredValidator(errorText: "กรุณาป้อนอีเมล"),
+                              EmailValidator(errorText: "รูปแบบอีเมลไม่ถูกต้อง")
+                            ]),
+                            keyboardType: TextInputType.emailAddress,
+                            onSaved: (String email) {
+                              profile.email = email;
+                            },
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text("รหัสผ่าน", style: TextStyle(fontSize: 20)),
+                          TextFormField(
+                            validator: RequiredValidator(
+                                errorText: "กรุณาป้อนรหัสผ่าน"),
+                            obscureText: true,
+                            onSaved: (String password) {
+                              profile.password = password;
+                            },
+                          ),
+                          SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                child: Text("ลงทะเบียน",
+                                    style: TextStyle(fontSize: 20)),
+                                onPressed: () {
+                                  if (formKey.currentState.validate()) {
+                                    formKey.currentState.save();
+                                    print(
+                                        "email = ${profile.email} password = ${profile.password}");
+                                    formKey.currentState.reset();
+                                  }
+                                },
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
           }
           return Scaffold(
             body: Center(
@@ -37,7 +93,5 @@ class _RegisterScreenState extends State<RegisterScreen> {
             ),
           );
         });
-
-    
   }
 }
